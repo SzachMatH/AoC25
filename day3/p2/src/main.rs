9287be2ef94d2b1435efc6a31e550bf6a2ecec5b
SzachMatH <szach.bart@gmail.com>
2025-12-03 11:25:34 +0100

    solved second part of day3

--- day3/p2/src/main.rs
+++ day3/p2/src/main.rs
@@ -1,24 +1,36 @@
 use std::fs;
 
+const BANK_SIZE : i64 = 100;
+
+fn recursion(from : usize, left : i64, arr : &[u8; BANK_SIZE as usize]) -> i64 {
+    let mut maxx = b'0';
+    let mut position = 0;
+    for i in from..(BANK_SIZE as usize -left as usize) {
+        if maxx < arr[i] {
+            maxx = arr[i];
+            position = i;
+        }
+    }
+    if left == 0 {
+        return (maxx - b'0') as i64;
+    }
+    10i64.pow(left as u32)*((maxx - b'0') as i64 ) + recursion(position+1, left - 1, arr)
+}
+
 fn main() {
     let input = fs::read_to_string("input").unwrap();
-    let banks : Vec<&[u8]> = input
+    let banks : Vec<[u8; BANK_SIZE as usize]> = input
         .trim()
         .split_whitespace()
-        .map(|a| a.as_bytes())
+        .map(|a| {
+            let bytes = a.as_bytes();
+            bytes.try_into().unwrap()
+        })
         .collect();
     
-    let mut sum : i32 = 0;
+    let mut sum : i64 = 0;
     for bank in banks {
-        let mut bigest = b'0';
-        let mut add = 0;
-        for el in bank.iter() {
-            add = u8::max(add,(bigest - b'0')*10 + (el - b'0'));
-            if bigest < *el {
-                bigest = *el;
-            }
-        } 
-        sum += add as i32;
+        sum += recursion(0,11, &bank);
     }
     println!("sum is {}",sum);
 }
